4b3ac439b702b46cc939f671891705fa39c57b09
TomGroombridge <groombridgetom@gmail.com>
2013-11-29 16:32:37 +0000

    plane attributes added

---
+++ planes.rb
@@ -0,0 +1,29 @@
+class Planes
+
+	def initialize
+       @landed = true
+       @not_cleared = true
+	end
+
+    def landed?
+      @landed
+      
+    end 
+
+    def not_cleared?
+       @not_cleared 
+    end
+
+    def ready
+    	@not_cleared = false
+    end
+
+    def take_off
+        @landed = false
+    end
+
+    def land
+        @landed = true
+    end
+
+end
