159f086725facf4332ccd13f0ec85a9a798275be
TomGroombridge <groombridgetom@gmail.com>
2013-11-29 16:33:08 +0000

    airport attributes added

---
+++ airport.rb
@@ -0,0 +1,26 @@
+class Airport
+
+	def initialize  
+		@capcaity = 20 
+	end
+
+	def planes
+		@planes ||= []
+	end
+
+	def plane_count
+		planes.count
+	end
+
+	def dock(plane)
+		raise "airport is full" if full?
+		planes << plane
+	end
+
+	def full?
+		plane_count == @capcaity
+	end
+
+
+
+end
